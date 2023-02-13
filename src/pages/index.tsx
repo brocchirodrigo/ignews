344bba106b28a497edfb3bc6eeb6218d45dc496a
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2023-02-13 19:53:11 -0300

    Static home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 import Image from "next/image";
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1Mb8raLTI7wXcRVSGMtYNBZr", {
     expand: ["product"],
   });
@@ -66,5 +66,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 horas
   };
 };
